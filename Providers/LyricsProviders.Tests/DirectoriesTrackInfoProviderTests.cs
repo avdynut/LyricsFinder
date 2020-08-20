8097ddf21a220836c077be109fb51e047a84e612
Andrey Arekhva <a.arekhva@hotmail.com>
2020-08-20 21:21:18 +0300

    Lyrics provider for local directories

---
+++ Providers/LyricsProviders.Tests/DirectoriesTrackInfoProviderTests.cs
@@ -0,0 +1,62 @@
+﻿using LyricsFinder.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LyricsProviders.Tests
+{
+    [TestClass]
+    public class DirectoriesTrackInfoProviderTests
+    {
+        [TestMethod]
+        public void CreateNotExistingFoldersTest()
+        {
+            var folders = new List<string> { "1", "2", "3" };
+            var provider = new DirectoriesTrackInfoProvider(folders);
+
+            Assert.IsNotNull(provider.LyricsFolders);
+            Assert.AreEqual(0, provider.LyricsFolders.Count, "Folders are not exist");
+        }
+
+        [TestMethod]
+        public async Task FindLyricsTest()
+        {
+            var folders = new List<string> { "a", "b", "c" };
+
+            foreach (var folder in folders)
+            {
+                Directory.CreateDirectory(folder);
+                var guid = Guid.NewGuid().ToString();
+                await File.WriteAllTextAsync(Path.Combine(folder, guid), guid);
+            }
+
+            const string artist = "Ben";
+            const string title = "Song";
+
+            var lyricsFile = Path.Combine(folders[1], $"{artist.ToUpper()} - {title.ToLower()}.txt");
+            var lyricsText = "blablabla";
+            await File.WriteAllTextAsync(lyricsFile, lyricsText);
+
+            var trackInfo = new TrackInfo { Artist = artist, Title = title };
+
+            var provider = new DirectoriesTrackInfoProvider(folders);
+
+            Assert.IsNotNull(provider.LyricsFolders);
+            Assert.AreEqual(folders.Count, provider.LyricsFolders.Count);
+
+            var track = await provider.FindTrackAsync(trackInfo);
+
+            Assert.IsNotNull(track.Lyrics);
+            Assert.AreEqual(lyricsText, track.Lyrics.Text);
+            Assert.AreEqual(artist, track.Artist);
+            Assert.AreEqual(title, track.Title);
+
+            foreach (var folder in folders)
+            {
+                Directory.Delete(folder, true);
+            }
+        }
+    }
+}
---
+++ Providers/LyricsProviders/DirectoriesTrackInfoProvider.cs
@@ -0,0 +1,62 @@
+﻿using LyricsFinder.Core;
+using LyricsFinder.Core.LyricTypes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LyricsProviders
+{
+    public class DirectoriesTrackInfoProvider : ITrackInfoProvider
+    {
+        private const string ArtistMask = "%Artist";
+        private const string TitleMask = "%Title";
+
+        public string Name => "Directories";
+
+        public string SearchPattern { get; }
+        public List<DirectoryInfo> LyricsFolders { get; } = new List<DirectoryInfo>();
+
+        public DirectoriesTrackInfoProvider(IEnumerable<string> lyricsFolders, string searchPattern = "%Artist - %Title")
+        {
+            SearchPattern = searchPattern ?? throw new ArgumentNullException(nameof(searchPattern));
+
+            foreach (var folder in lyricsFolders)
+            {
+                var directory = new DirectoryInfo(folder);
+                if (directory.Exists)
+                {
+                    LyricsFolders.Add(directory);
+                }
+            }
+        }
+
+        public async Task<Track> FindTrackAsync(TrackInfo trackInfo)
+        {
+            var track = new Track(trackInfo);
+
+            foreach (var directory in LyricsFolders)
+            {
+                var enumerationOptions = new EnumerationOptions
+                {
+                    MatchCasing = MatchCasing.CaseInsensitive,
+                    RecurseSubdirectories = true
+                };
+
+                var pattern = SearchPattern.Replace(ArtistMask, trackInfo.Artist).Replace(TitleMask, trackInfo.Title);
+                foreach (var file in directory.EnumerateFiles(pattern + "*", enumerationOptions))
+                {
+                    var lyrics = await File.ReadAllTextAsync(file.FullName);
+                    if (lyrics.Length > 0)
+                    {
+                        track.Lyrics = new UnsyncedLyric(lyrics);
+                        return track;
+                    }
+                }
+            }
+
+            track.Lyrics = new NoneLyric("Not Found");
+            return track;
+        }
+    }
+}
