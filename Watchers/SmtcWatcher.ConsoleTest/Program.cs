33b7d1a3ee40399348d67766935d0521879b50bf
Andrey Arekhva <a.arekhva@gmail.com>
2020-11-28 17:39:53 +0300

    Add projects for SMTC watcher

---
+++ Watchers/SmtcWatcher.ConsoleTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SmtcWatcher.ConsoleTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var smtcWatcher = new SystemMediaWatcher();
+            await smtcWatcher.InitializeAsync();
+
+            while (true)
+            {
+
+            }
+            smtcWatcher.Dispose();
+        }
+    }
+}
---
+++ Watchers/SmtcWatcher/SystemMediaWatcher.cs
@@ -0,0 +1,78 @@
+﻿using NLog;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Media.Control;
+
+namespace SmtcWatcher
+{
+    /// <summary>
+    /// Fetches media info from system media transport controls.
+    /// </summary>
+    public class SystemMediaWatcher : IDisposable
+    {
+        private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
+
+        private GlobalSystemMediaTransportControlsSessionManager _sessionManager;
+        private GlobalSystemMediaTransportControlsSession _currentSession;
+
+        public SystemMediaWatcher()
+        {
+            //    var sessions = smtcSessionManager.GetSessions();
+            //    foreach (var session in sessions)
+            //    {
+            //        var info = session.GetPlaybackInfo();
+            //        var time = session.GetTimelineProperties();
+            //        var d = await session.TryGetMediaPropertiesAsync();
+            //    }
+        }
+
+        public async Task InitializeAsync()
+        {
+            _logger.Debug("Initialize session manager");
+
+            _sessionManager = await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
+
+            GetSessions();
+            GetCurrentSession();
+
+            _sessionManager.SessionsChanged += OnSessionsChanged;
+            _sessionManager.CurrentSessionChanged += OnCurrentSessionChanged;
+        }
+
+        private void GetSessions()
+        {
+            var sessions = _sessionManager.GetSessions();
+            _logger.Debug($"Sessions: {string.Join(", ", sessions.Select(x => x.SourceAppUserModelId))}");
+        }
+
+        private void GetCurrentSession()
+        {
+            _currentSession = _sessionManager.GetCurrentSession();
+            _logger.Debug($"Current session: {_currentSession?.SourceAppUserModelId}");
+
+            if (_currentSession != null)
+            {
+                //_currentSession.
+            }
+        }
+
+        private void OnSessionsChanged(GlobalSystemMediaTransportControlsSessionManager sender, SessionsChangedEventArgs args)
+        {
+            GetSessions();
+        }
+
+        private void OnCurrentSessionChanged(GlobalSystemMediaTransportControlsSessionManager sender, CurrentSessionChangedEventArgs args)
+        {
+            GetCurrentSession();
+        }
+
+        public void Dispose()
+        {
+            _sessionManager.SessionsChanged -= OnSessionsChanged;
+            _sessionManager.CurrentSessionChanged -= OnCurrentSessionChanged;
+
+            _logger.Debug("Disposed session manager");
+        }
+    }
+}
