6c44ff3008a2d38aa7329a1201fe47aaa91ad3cd
Andrey Arekhva <a.arekhva@gmail.com>
2020-12-21 13:59:16 +0300

    use global session manager

--- LyrixoundModern/MainPage.xaml.cs
+++ LyrixoundModern/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Control;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,16 @@ namespace LyrixoundModern
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var _sessionManager = await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
+            var _currentSession = _sessionManager.GetCurrentSession();
+            if (await _currentSession?.TryGetMediaPropertiesAsync() is GlobalSystemMediaTransportControlsSessionMediaProperties mp)
+            {
+
+            }
         }
     }
 }
